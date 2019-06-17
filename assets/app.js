6bc4b5836c639b07f99525d6ee03700761cff95b
“[Yelena <[merzlyakova.lena@gmail.com]>
2019-06-17 13:48:57 +0200

    add computer choice

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,7 @@
 let rock = document.getElementById('choice1');
 let paper = document.getElementById('choice2');
 let scissors = document.getElementById('choice3');
+let myChoice;
 
 rock.addEventListener("click", function () {
     gamble(rock);
@@ -14,14 +15,93 @@ scissors.addEventListener("click", function () {
     gamble(scissors);
 });
 
-function gamble() {
-    if (rock){
-        console.log(rock);
+let playerChoice = gamble();
+
+function gamble(choice) {
+    if (choice == rock) {
+        myChoice = "rock";
     }
-    else if (paper){
-        console.log(paper);
+    else if (choice == paper) {
+        myChoice = "paper";
     }
-    else{
-        console.log(scissors);
+    else {
+        myChoice = "scissors";
     }
+    console.log(myChoice);
 }
+
+let computer = document.getElementById('computerGamble');
+let computerChoice = Math.random();
+
+computer.addEventListener("click", function () {
+    var computerChoice = Math.random();
+    if (computerChoice < 0.34) {
+        computerChoice = "rock";
+    }
+    else if (computerChoice <= 0.67) {
+        computerChoice = "paper";
+    }
+    else {
+        computerChoice = "scissors";
+    }
+
+    compare(computerChoice);
+    console.log("Computer choses " + computerChoice);
+});
+
+
+// /*let result = document.getElementById('result');*/
+
+// let player = document.getElementById('letsPlay');
+// player.addEventListener("click", function () {
+//     compare();
+// });
+function compare() {
+    
+    if (myChoice === computerChoice) {
+        
+        return "It's a draw!" + " " + "Lets play again." ;
+        console.log('a');
+    }
+    
+    else if (myChoice === "rock") {
+        
+        if (computerChoice === "scissors") {
+            return "Rock wins" + "<br>" + "You win!";
+            console.log('b');
+        }
+        else {
+            return "Paper wins" + "<br>" + "You lose!";
+            console.log('c');
+        }
+    }
+    
+    else if (myChoice === "paper") {
+        
+        if (computerChoice === "rock") {
+            return "Paper wins" + "<br>" + "You win!";
+            console.log('d');
+        }
+        else {
+            return "Scissors win" + "<br>" + "You lose!";
+            console.log('e');
+        }
+            
+    }
+    
+    else if (myChoice === "scissors") {
+        
+        if (computerChoice === "rock") {
+            return "rock wins" + "<br>" + "You lose!";
+            console.log('f');
+        }
+        else {
+            return "scissors win" + "<br>" + "You win!";
+            console.log('g');
+        }
+        console.log('a')
+    }
+    
+    
+    } 
+   document.write(compare(myChoice,computerChoice));
